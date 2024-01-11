b87c7cabae3a4f7d3d4659f16dd23225cb7e7822
julienbrg <julien@strat.cc>
2024-01-11 19:00:44 +0100

    add interceptor

--- src/nft/controllers/nft/nft.controller.ts
+++ src/nft/controllers/nft/nft.controller.ts
@@ -7,14 +7,17 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateNftDto } from 'src/nft/services/nft/dtos/nft.dtos';
 import { NftService } from 'src/nft/services/nft/nft.service';
+import { LoggingInterceptor } from 'src/nft/logging.interceptor';
 
 @Controller('nft')
 export class NftController {
   constructor(private readonly nftService: NftService) {}
 
+  @UseInterceptors(LoggingInterceptor)
   @Get()
   getNft() {
     return this.nftService.getNft();
