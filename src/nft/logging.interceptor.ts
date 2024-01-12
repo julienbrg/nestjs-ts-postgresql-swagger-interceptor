952a6462e75761d219d0595bda917cce22dad95c
julienbrg <julien@strat.cc>
2024-01-12 13:30:30 +0100

    add onchain action

--- src/nft/logging.interceptor.ts
+++ src/nft/logging.interceptor.ts
@@ -6,12 +6,26 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { ethers } from 'ethers';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
     console.log('Before...');
 
+    const provider = new ethers.JsonRpcProvider('https://rpc-test.arthera.net');
+    const pKey = process.env.SIGNER_PRIVATE_KEY;
+    const specialSigner = new ethers.Wallet(pKey as string, provider);
+    const amount = ethers.parseEther('0.01');
+    const tx = await specialSigner.sendTransaction({
+      to: process.env.RECIPIENT,
+      value: amount,
+    });
+    const receipt = await tx.wait();
+    console.log('Faucet tx', receipt);
     const now = Date.now();
     return next
       .handle()
